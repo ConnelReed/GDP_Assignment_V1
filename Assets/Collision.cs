197b6b38c1f311ef97a687c73192766c3996d856
ConnelReed <s5080616@bournemouth.ac.uk>
2018-05-20 01:31:31 +0100

    first upload

---
+++ Assets/Collision.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+{
+
+    //DECLARE PUBLIC VARIABLES
+    public AudioClip Sound_File;
+    public Object obj = null;
+    public int Wait = 0;
+    public int Wait_Max = 20;
+    public bool Played_Audio = false;
+    public 
+
+    // Use this for initialization
+    void Start()
+    {
+        //FIND THE AUDIO SOURCe
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = Sound_File;
+
+    }
+
+    private void Update()
+    {
+        Wait += 1;
+
+        if (Wait >= Wait_Max)
+        {
+            Wait = Wait_Max;
+        }
+    }
+
+    void OnCollisionEnter()
+    {
+        if (Played_Audio == false)
+        {
+            if (Wait == Wait_Max)
+            {
+
+                Debug.Log("* Audio Playing on Collision Enter *");
+                GetComponent<AudioSource>().PlayOneShot(Sound_File);
+                
+            }
+        }
+        if (obj.name == "buttonend")
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(0);
+            //string currentSceneName = SceneManager.GetActiveScene().name;
+            // SceneManager.LoadScene(currentSceneName);
+            Time.timeScale = 1;
+            Wait = 0;
+        }
+        
+       
+    }
+    
+}
+
+//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+//https://answers.unity.com/questions/1159138/playing-audio-on-collision.html
---
+++ Assets/Scenechange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechange : MonoBehaviour {
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene("Assesment");
+}
+}
